114a0efc0fbca3c19ebfb7368a711f2a565b8ba5
LuisManuelPozoDLT6 <20203tn047@utez.edu.mx>
2023-11-28 13:07:30 -0600

    tiendas listo

--- Front/assets/js/products/products.js
+++ Front/assets/js/products/products.js
@@ -4,7 +4,7 @@ const getProducts = async () => {
         const response = await axiosClient.get(`/product/`)
         response.data.forEach(product => {
             products += `
-            <div class="col-md-4 col-sm-6 " onclick="getProductById(1)">
+            <div class="col-md-4 col-sm-6 ">
                 <div class="card">
                     <div class="card-body text-center">
                         <img src="../../assets/images/paletas.png" width="150px" height="150px" class="img-cover" alt="">
@@ -12,7 +12,7 @@ const getProducts = async () => {
                         <h5 style="margin-top: -5px;">${'$' +product.price}</h5>
                         <p class="text-secondary mrl-3">${product.description}</p>
                         <div class="d-flex justify-content-end">
-                            <button type="button" class="btn btn-morado btn-circle"><i class="fa-solid fa-pen"></i></button>
+                            <button type="button" onclick="getProductById(${product.id})" class="btn btn-morado btn-circle"><i class="fa-solid fa-pen"></i></button>
                         </div>
                     </div>
                 </div>
@@ -25,6 +25,53 @@ const getProducts = async () => {
         console.log(error);
     }
 }
+
+const updateProduct = async () => {
+    let id = document.getElementById('idUpdate').value;
+
+    let product = {
+        "id": document.getElementById('idUpdate').value,
+        "name": document.getElementById('nameUpdate').value,
+        "description": document.getElementById('descriptionUpdate').value,
+        "price": document.getElementById('priceUpdate').value
+    }
+
+    try {
+        const response = await axiosClient.put(`/product/`, product);
+        $('#productModalUpdate').modal('hide');
+        getProducts();
+        Swal.fire({
+            icon: "success",
+            title: "Se modificó el producto",
+            showConfirmButton: false,
+            timer: 1500
+        });
+    } catch (error) {
+        console.log(error);
+        Swal.fire({
+            icon: "error",
+            title: "No fue posible modificar el producto",
+            showConfirmButton: false,
+            timer: 1500
+        });
+    }
+}
+
+const getProductById = async (id) => {
+    try {
+        const response = await axiosClient.get(`/product/${id}`);
+        document.getElementById('idUpdate').value = response.data.id;
+        document.getElementById('nameUpdate').value = response.data.name;
+        document.getElementById('priceUpdate').value = response.data.price;
+        document.getElementById('descriptionUpdate').value = response.data.description;
+        console.log(response);
+
+        $('#productModalUpdate').modal('show');
+    } catch (error) {
+        console.log(error);
+    }
+};
+
 
 const saveProduct = async () => {
     let product = {
@@ -41,9 +88,21 @@ const saveProduct = async () => {
             cleanForm();
             getProducts();
             $('#productModal').modal('hide');
+            Swal.fire({
+                icon: "success",
+                title: "Se registró el producto",
+                showConfirmButton: false,
+                timer: 1500
+            });
         })
         .catch(error => {
             console.error('Error en la solicitud POST:', error);
+            Swal.fire({
+                icon: "error",
+                title: "No fue posible registrar el producto",
+                showConfirmButton: false,
+                timer: 1500
+            });
         });
     
 }
@@ -53,3 +112,6 @@ const cleanForm = () => {
     document.getElementById('description').value = "";
     document.getElementById('price').value = "";
 }
+
+
+
--- Front/assets/js/store/stores.js
+++ Front/assets/js/store/stores.js
@@ -4,6 +4,7 @@ const getStores = async () => {
     let stores = ``;
     try {
         const response = await axiosClient.get(`/store/`);
+
         response.data.forEach((store, index) => {
             stores += `
             <div class="col-md-4 col-sm-6 mb-5">
@@ -28,18 +29,111 @@ const getStores = async () => {
     }
 }
 
+const saveOwner = async (owner) => {
+    let ownerRegistered;
+    try {
+        response = await axiosClient.post(`/person/`, owner);
+        ownerRegistered = response.data;
+    } catch (error) {
+        console.log(error);
+    }
+    return ownerRegistered;
+}
+
+const getById = async () => {
+    const storeId = localStorage.getItem('selectedStoreId');
+    try {
+        const responseDeliver = await axiosClient.get(`/person/`);
+        console.log(responseDeliver);
+        let selectDelivers = $("#delivers");
+        selectDelivers.append(`<option value="0">Señeccione un repartidor</option>`)
+        const response = await axiosClient.get(`/store/${storeId}`);
+
+        for (let i = 0; i < responseDeliver.data.length; i++) {
+            if (responseDeliver.data[i].id === responseDeliver.data.id) {
+                option.selected = true;
+            }
+            selectDelivers.append(`<option value="${responseDeliver.data[i].id}">${responseDeliver.data[i].name} ${responseDeliver.data[i].lastName}</option>`)
+        }
+
+        document.getElementById('nameUpdate').value = response.data.name;
+        document.getElementById('addressUpdate').value = response.data.address;
+        document.getElementById('rfcUpdate').value = response.data.rfc;
+        document.getElementById('ownerNameUpdate').value = response.data.owner.name;
+        document.getElementById('phoneUpdate').value = response.data.owner.phone;
+        document.getElementById('idOwnerUpdate').value= response.data.owner.id;
+        document.getElementById('delivers').value= response.data.deliver.id;
+
+        $('#updateStoreModal').modal('show');
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+const updateStore = async () => {
+    const storeId = localStorage.getItem('selectedStoreId');
+    let owner = {
+        "id": document.getElementById('idOwnerUpdate').value,
+        "name": document.getElementById('ownerNameUpdate').value,
+        "phone": document.getElementById('phoneUpdate').value   
+    }
+    
+
+
+    try {
+        const responseOwner = await axiosClient.put(`/person/${owner.id}`, owner);
+
+        let store = {
+            "id": storeId,
+            "name": document.getElementById('nameUpdate').value,
+            "address": document.getElementById('addressUpdate').value,
+            "rfc": document.getElementById('rfcUpdate').value,
+            owner,
+            "deliver": {
+                "id": document.getElementById('delivers').value,
+            }
+        }
+
+        const response = await axiosClient.put(`/store/`, store);
+        $('#updateStoreModal').modal('hide');
+        getStoreById();
+        Swal.fire({
+            icon: "success",
+            title: "Se modificó la tienda",
+            showConfirmButton: false,
+            timer: 1500
+        });
+    } catch (error) {
+        console.log(error);
+        Swal.fire({
+            icon: "error",
+            title: "No fue posible modificar la tienda",
+            showConfirmButton: false,
+            timer: 1500
+        });
+    }
+}
+
 const saveStore = async () => {
+    let owner = {
+        "name": document.getElementById('ownerName').value,
+        "phone": document.getElementById('phone').value,
+    }
+
+    const ownerRegistered = await saveOwner(owner);
+
     let store = {
         "name": document.getElementById('name').value,
         "address": document.getElementById('address').value,
         "rfc": document.getElementById('rfc').value,
         "owner": {
-            "id" : 1
+            "id" : ownerRegistered.id
         },
         "deliver": {
             "id": document.getElementById('delivers').value,
         }
     }
+
 
     console.log(store);
     axiosClient.post(`/store/`, store)
@@ -48,9 +142,21 @@ const saveStore = async () => {
             cleanForm();
             getStores();
             $('#saveStoreModal').modal('hide');
+            Swal.fire({
+                icon: "success",
+                title: "Se registró la tienda",
+                showConfirmButton: false,
+                timer: 1500
+            });
         })
         .catch(error => {
             console.error('Error en la solicitud POST:', error);
+            Swal.fire({
+                icon: "error",
+                title: "No fue posible registrar la tienda",
+                showConfirmButton: false,
+                timer: 1500
+            });
         });
     
 }
@@ -85,12 +191,26 @@ const getDelivers = async () => {
     }
 }
 
+const getVisits = async (id) => {
+    let visits;
+    try {
+        const response = await axiosClient.get(`/visits/store/${id}`);
+        visits = response.data;
+    } catch (error) {
+        console.log(error);
+    }
+    return visits;
+}
+
 const getStoreById = async () => {
     const storeId = localStorage.getItem('selectedStoreId');
     let store = ``;
     let repartidor = ``;
+    let visitCard = ``;
     try {
         const response = await axiosClient.get(`/store/${storeId}`);
+        const visits = await getVisits(storeId);
+        console.log(visits);
         console.log(response.data);
         store = `
         <div class="card bg-morado card-store-large shadow align-items-center ">
@@ -115,20 +235,36 @@ const getStoreById = async () => {
             <div class="flex-grow-1 ms-3">
                 <h4 class="d-flex align-items-center justify-content-between">
                 <span class="fw-bold">Repartidor asignado</span>
-                <i style="font-size: 20px;" class="fa-solid fa-pen-to-square"></i>
+                <i onclick="getById()" style="font-size: 20px;" class="fa-solid fa-pen-to-square"></i>
                 </h4>
                 <h6 class="fw-lighter" style="font-size: 17px;">${response.data.deliver.name + ' ' + response.data.deliver.lastName}</h6>
             </div>
         `
 
-        response.data.visitList.forEach((visit, index) => {
-
+        visits.forEach((visit, index) => {
+            visitCard += `
+            <div class="col-md-4 col-6 mb-4 " >
+                <div class="card shadow card-visit" data-bs-toggle="modal" style="height:200px;" data-bs-target="#exampleModal">
+                    <div class="card-body text-center overflow-auto">
+                        <h3 class="circle-visit mx-auto ${index % 2 == 0 ? 'bg-rosa' : 'bg-morado'}"><i class="fa-solid fa-calendar-days"></i></h3>
+                        <p>${visit.day_visit}</p>
+                        <span class="badge bg-success">${visit.status.desciprtion}</span>
+                    </div>
+                </div>
+            </div>
+            `
         });
+        console.log(visits.length);
 
         document.getElementById('store').innerHTML = store;
         document.getElementById('deliver').innerHTML = repartidor;
+        document.getElementById('visits').innerHTML = visitCard;
     } catch (error) {
         console.log(error);
     }
+}
+
+const hola= () =>{
+    console.log('hello world');
 }
 
